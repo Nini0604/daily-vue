ed2a29f80a3c033f1445cc6356271d3dcc46951e
nini <nini@marketdesigners.com>
2020-01-24 05:00:16 +0900

    200123: 틱택토 및 index페이지 수정

---
+++ pages/200123/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <TableComponent :tableData="tableData" />
+    <p>{{ turn }}님 차례입니다</p>
+    <p v-if="winner">
+      {{ winner }} 님이 승리하셨습니다
+    </p>
+  </div>
+</template>
+
+<script>
+import TableComponent from '~/components/200123/TableComponent.vue'
+export default {
+  components: { TableComponent },
+  data() {
+    return {
+      tableData: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+      ],
+      turn: 'O',
+      winner: ''
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+  p {
+    font-size: 20px;
+  }
+</style>
